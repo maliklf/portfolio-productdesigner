0d3737169fa06db7fd3b589f75520e730457a9db
malikpages <malik.workfile@gmail.com>
2025-03-22 17:51:15 +0900

    Add contact me section (mobile)

--- src/script.js
+++ src/script.js
@@ -5,28 +5,3 @@ const buttonToogle = document.querySelector('.buttonToogle');
         buttonToogle.addEventListener('click', () => {
             mobileMenu.classList.toggle('hidden');
         });
-
-//Carousel
- var swiper = new Swiper(".multiple-slide-carousel", {
-   loop: true,
-   slidesPerView: 3,
-   spaceBetween: 20,
-   navigation: {
-     nextEl: ".multiple-slide-carousel .swiper-button-next",
-     prevEl: ".multiple-slide-carousel .swiper-button-prev",
-   },
-   breakpoints: {
-    1920: {
-        slidesPerView: 3,
-        spaceBetween: 30
-    },
-    1028: {
-        slidesPerView: 2,
-        spaceBetween: 30
-    },
-    990: {
-        slidesPerView: 1,
-        spaceBetween: 0
-    }
-  }
- });
